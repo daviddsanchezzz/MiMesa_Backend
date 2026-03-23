23b8b692ae3ae70970bc75f1683eb34d89e68467
DavidSanchez287761 <287761@student@pwr.edu.pl>
2026-03-23 18:07:09 +0100

    werf

--- routes/dev.js
+++ routes/dev.js
@@ -2,19 +2,15 @@ const router         = require('express').Router();
 const requireSession = require('../middleware/requireSession');
 const requireDev     = require('../middleware/requireDev');
 const c              = require('../controllers/devController');
-const pc             = require('../controllers/pricingController');
 
 // All dev routes require a valid session AND dev email
 router.use(requireSession, requireDev);
 
-router.get('/businesses',          c.listBusinesses);
-router.post('/businesses',         c.createBusiness);
+router.get('/businesses',            c.listBusinesses);
+router.post('/businesses',           c.createBusiness);
 router.patch('/businesses/:id/plan', c.updatePlan);
-router.delete('/businesses/:id',   c.deleteBusiness);
-router.post('/migrate-memberships', c.migrateMemberships);
-router.post('/invite-user', c.inviteUser);
-
-router.get('/pricing', pc.getDevPricing);
-router.put('/pricing', pc.upsertPricing);
+router.delete('/businesses/:id',     c.deleteBusiness);
+router.post('/migrate-memberships',  c.migrateMemberships);
+router.post('/invite-user',          c.inviteUser);
 
 module.exports = router;
